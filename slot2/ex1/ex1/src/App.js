c5d97799975a105c37eae5fdd0bb4a1a871447a9
DoanNhat219 <tranphuocdoannhat.2005@gmail.com>
2026-05-13 16:02:01 +0700

    update ex1

---
+++ slot2/ex1/ex1/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>DoanNhat</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.facebook.com/oannhat.713686/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         My Facebook
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
